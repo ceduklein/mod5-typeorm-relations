4f75f25a5a2cfe4e8271cb89efb76237c38a9c0d
Carlos Klein <c.edudepaula@yahoo.com.br>
2020-06-12 17:31:21 -0300

    desafio finalizado

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,62 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customerExists = await this.customersRepository.findById(customer_id);
+    if (!customerExists) {
+      throw new AppError('Customer not found.');
+    }
+
+    const checkProducts = await this.productsRepository.findAllById(products);
+    if (checkProducts.length < products.length) {
+      throw new AppError('One or more products are not found.');
+    }
+
+    const checkQuantity = products.filter(
+      product =>
+        checkProducts.filter(prod => prod.id === product.id)[0].quantity <=
+        product.quantity,
+    );
+
+    if (checkQuantity.length) {
+      throw new AppError(
+        'One or more products does not have the requested quantity',
+      );
+    }
+
+    const productsOrder = products.map(product => ({
+      product_id: product.id,
+      quantity: product.quantity,
+      price: checkProducts.filter(prod => prod.id === product.id)[0].price,
+    }));
+
+    const order = await this.ordersRepository.create({
+      customer: customerExists,
+      products: productsOrder,
+    });
+
+    const { order_products } = order;
+
+    const orderedQuantity = order_products.map(product => ({
+      id: product.product_id,
+      quantity:
+        checkProducts.filter(prod => prod.id === product.product_id)[0]
+          .quantity - product.quantity,
+    }));
+
+    await this.productsRepository.updateQuantity(orderedQuantity);
+
+    return order;
   }
 }
 
---
+++ src/shared/infra/typeorm/migrations/1591918878520-CreateOrder.ts
@@ -0,0 +1,56 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class CreateOrder1591918878520 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'orders',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'customer_id',
+            type: 'uuid',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'orders',
+      new TableForeignKey({
+        name: 'orderCustomerId',
+        columnNames: ['customer_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'customers',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('orders', 'orderCustomerId');
+    await queryRunner.dropTable('orders');
+  }
+}
